9b6a311e66d3fab1e0b66332b59b9dc1b98fd1ee
YERNAZAR NURTAY <154218708+Ernazar1990@users.noreply.github.com>
2026-07-14 11:17:16 +0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SmartUBT from "./SmartUBT.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SmartUBT />
-  </React.StrictMode>
-);
